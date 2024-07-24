c9381f998ada3ca7f146fcaf61d067c76cd12ad0
jvkchow <jechow@ualberta.ca>
2024-07-24 00:36:39 -0600

    updated dependencies

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -2,111 +2,141 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+
+      {/* Home Section */}
+      <div>
+        <div>Hello, my name is</div>
+        <div>Jan Chow</div>
+        <div>And I'm a <span>computer science student</span></div>
+      </div>
+
+      {/* About Section */}
+      <div>
+        <h2>About Me</h2>
+        <div>
+          {/* Column Left */}
+          <div>
+            <img src="static/profile.jpg" alt=""></img>
+          </div>
+          {/* Column Right */}
+          <div>
+            <div>I'm Jan and I'm a <span>computer science student</span></div>
+            <p>
+              I am a graduate of the University of Alberta with a Bachelor's degree in Specialization in Computing Science.<br></br>
+              Outside of academics, I work at Code Ninjas where I teach kids how to code and help foster their STEM knowledge and skills. I also enjoy going out to eat good food and to explore new tastes. 
+              <br></br><br></br>I am currently searching for work related to computing science. Please feel free to contact me if you are interested in having me join
+              your team!
+            </p>
+            <a href="https://drive.google.com/file/d/1NfGUr5Wo5WawRw7UZgJjjnC3oSdcHbH6/view?usp=sharing" target = "_blank">Open Resume</a>
+          </div>
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* Projects Section */}
+      <div>
+        <h2>My Projects</h2>
+        <h3>Click each card to access the source code on GitHub.</h3>
+        <div>
+          <div> {/*Card One*/}
+            <a href="https://github.com/jvkchow/JanChowWebsite" target = "_blank">
+            <div>
+              <i></i>
+                <div>This Website</div>
+                  <p>Website to display my frontend skills and to show more about myself in an easy to read format.<br></br><br></br>
+                                  [Uses: HTML, CSS, JavaScript]
+                  </p>
+            </div>
+            </a>
+          </div>
+          <div> {/*Card Two*/}
+            <a href="https://github.com/CMPUT301W22T17/CMPUT301_GroupProject" target = "_blank">
+            <div>
+              <i></i>
+                <div>SuperQR</div>
+                  <p>Pokemon GO but if you caught QR codes instead of Pokemon.</p>
+                  <br></br>Link to demo video:<br></br>
+                  <div>
+                      <a href="https://www.youtube.com/watch?v=_7kmTEr8-Hs" target = "_blank">Click Here</a>
+                  </div>
+                  <p><br></br>[Uses: Java, XML]</p>
+            </div>
+            </a>
+          </div>
+          <div> {/*Card Three*/}
+            <a href="https://github.com/jvkchow/MemoryGame" target = "_blank">
+            <div>
+              <i></i>
+                <div>Memory Game</div>
+                  <p>Classic memory game. Flip tiles, challenge your brain. Win.<br></br><br></br>
+                    [Uses: Python]
+                  </p>
+            </div>
+            </a>
+          </div>
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      {/* Skills Section */}
+      <div>
+        <h2>My Skills</h2>
+        <div>
+          <div> {/*Card one*/}
+            <div>
+              <i></i>
+              <div>Backend</div>
+              <p><br></br>Python &nbsp;&nbsp;· &nbsp;&nbsp;Java &nbsp;&nbsp;· &nbsp;&nbsp;C &nbsp;&nbsp;· &nbsp;&nbsp;SQL</p>
+            </div>
+          </div>
+          <div>
+            <i></i>
+          </div>
+          <div> {/* Card Two*/}
+            <div>
+              <i></i>
+              <div>Frontend</div>
+              <p><br></br>HTML &nbsp;&nbsp;· &nbsp;&nbsp;CSS &nbsp;&nbsp;· &nbsp;&nbsp;JavaScript</p>
+            </div>
+          </div>
+        </div>
+      </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      {/* Contact Section */}
+      <div>
+        <h2>Contact Me</h2>
+        <div>
+          <div> {/* Column Left */}
+            <div>Get in Touch</div>
+            <p>If you have any further questions or would like to offer me a position in your team, please do not hestitate to contact me at my phone number or email below.</p>
+            <div>
+              <div>
+                <i></i>
+                <div>
+                  <div>Name</div>
+                  <div>Jan Chow</div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div>
+                <i></i>
+                <div>
+                  <div>Phone Number</div>
+                  <div>(780) 935-2309</div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div>
+                <i></i>
+                <div>
+                  <div>Email</div>
+                  <div>jechwk@gmail.com</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
